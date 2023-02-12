0089e32568497564b23d6fe083cc9ce74bde40e1
mjkoster <michaeljohnkoster@gmail.com>
2023-02-11 19:38:16 -0800

    experiments folder

--- experiments/condition.js
+++ experiments/condition.js
@@ -11,8 +11,10 @@ const expression = "In_1 == 1 and In_2 == 1 and In_3 == 0";
 
 const ast = math.parse(expression);
 
+//console.log(ast);
+
 const result = ast.evaluate(scope);
 
-console.log(math.parse(expression).evaluate(scope));
+console.log(result);
 
 // console.log((require('mathjs')).parse("In_1 == 1 and In_2 == 1 and In_3 == 0").evaluate({In_1:1,In_2:1,In_3:0}))
