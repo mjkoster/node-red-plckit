2dc7f62484e34d1e94450e9352413c22dc484604
mjkoster <michaeljohnkoster@gmail.com>
2023-02-12 21:21:28 -0800

    add condition logic

--- node-red-contrib-discrete-fsm/condition.js
+++ node-red-contrib-discrete-fsm/condition.js
@@ -1,10 +1,26 @@
 module.exports = function(RED) {
     function ConditionNode(config) {
         RED.nodes.createNode(this,config);
+
+        this.name = config.name;
+        this.expression = config.expression;
+        this.oninputtopic = config.oninputtopic;
+        this.onsynctopic = config.onsynctopic;
+
         var node = this;
         node.on('input', function(msg) {
-            msg.payload = msg.payload.toLowerCase();
-            node.send(msg);
+            if ( msg.topic == "input" || msg.topic == "init" ) {
+                for ( var input in msg.payload ) {
+                    node[input] = payload[input]
+                }
+            }
+            if (msg.topic == "input" && node.oninputtopic || msg.topic == "sync" && node.onsynctopic ) {
+                if ( mathjs.parse(node.expression).evaluate(node) ) {
+                    msg.topic = "condition"
+                    msg.payload = node.name;
+                    node.send(msg);
+                }
+            }
         });
     }
     RED.nodes.registerType("condition",ConditionNode);
