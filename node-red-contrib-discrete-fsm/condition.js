13b66f0cc025d514b024a31fb694f3be2c32d5b6
mjkoster <michaeljohnkoster@gmail.com>
2023-02-13 17:10:28 -0800

    Update condition.js

--- node-red-contrib-discrete-fsm/condition.js
+++ node-red-contrib-discrete-fsm/condition.js
@@ -12,28 +12,28 @@ module.exports = function(RED) {
         node.on('input', function(msg) {
             if ( msg.topic == "input" || msg.topic == "init" ) {
                 for ( var input in msg.payload ) {
-                    node.context().set(input, msg.payload[input]);
+                    node.context.set(input, msg.payload[input]);
                 }
             }
-            var context = {};
-            for ( var property in node.context().keys() ) {
-                context[property] = node.context().get(property);
-            };
-            msg.topic = "context";
-            msg.payload = context;
-            node.send(msg);
-    
             if (msg.topic == "input" && node.oninputtopic || msg.topic == "sync" && node.onsynctopic ) {
                 const condition =  mathjs.parse(expression);
                 const vnodes = condition.filter(isVariable);
                 var scope = {};
                 vnodes.forEach(vnode => {
-                    scope[vnode.name] = node.context().get(vnode.name);
+                    scope[vnode.name] = node.context.get(vnode.name);
                 });
+                
+                var context = {};
+                for ( var property in node.context.keys() ) {
+                    context[property] = node.context.get(property);
+                };        
+                msg.topic = "context";
+                msg.payload = context;
+                node.send(msg);
                 msg.topic = "scope";
                 msg.payload = scope;
                 node.send(msg);
-        
+
                 if ( condition.evaluate(scope) ) {
                     msg.topic = "condition"
                     msg.payload = node.name;
