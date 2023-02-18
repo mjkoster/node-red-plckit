81b8e8a7e4e79e561a68e573a882628290cc5d7f
mjkoster <michaeljohnkoster@gmail.com>
2023-02-18 08:21:49 -0800

    add the output node

---
+++ node-red-contrib-discrete-fsm/output.js
@@ -0,0 +1,26 @@
+module.exports = function(RED) {
+
+    function OutputNode(config) {        
+        RED.nodes.createNode(this,config);
+
+        var node = this;
+        
+        node.name = config.name;
+        node.rbe = config.rbe;
+        node.context.set('value', null);
+
+        node.on('input', function(msg) {
+            if(msg.topic == 'init' && node.name in msg.payload) {
+                node.context.set('value', msg.payload[node.name])
+            }
+            if(msg.topic == 'output' && node.name in msg.payload) {
+                if(!node.rbe || (node.rbe && ( msg.payload[node.name] != node.context.get('value')))) {
+                    node.context.set('value', msg.payload[node.name])
+                    msg.payload = node.context.get('value');
+                    node.send(msg);
+                }
+            }
+        });
+    }
+    RED.nodes.registerType("output",OutputNode);
+}
