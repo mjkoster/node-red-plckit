7ce0a458ab24ccbf48be67384f83d4ab72ff0152
mjkoster <michaeljohnkoster@gmail.com>
2023-03-11 12:39:41 -0800

    Update output.js
    status for boolean outputs

--- node-red-contrib-discrete-fsm/output.js
+++ node-red-contrib-discrete-fsm/output.js
@@ -9,14 +9,36 @@ module.exports = function(RED) {
         node.rbe = config.rbe;
         node.context().set('value', null);
 
+        node.status({});
+
         node.on('input', function(msg) {
             if(msg.topic == 'init' && node.name in msg.payload) {
-                node.context().set('value', msg.payload[node.name])
+                var value = msg.payload[node.name];
+                node.context().set('value', value)
+                if (typeof(value) == 'boolean') {
+                    if (value == true) {
+                        node.status({fill:"green",shape:"dot",text:"true"});
+                    } else {
+                        node.status({fill:"green",shape:"ring",text:"false"});
+                    }
+                } else {
+                    node.status({});
+                }    
             }
             if(msg.topic == 'output' && node.name in msg.payload) {
                 if(!node.rbe || (node.rbe && ( msg.payload[node.name] != node.context().get('value')))) {
-                    node.context().set('value', msg.payload[node.name])
-                    msg.payload = node.context().get('value');
+                    var value = msg.payload[node.name];
+                    node.context().set('value', value)
+                    if (typeof(value) == 'boolean') {
+                        if (value == true) {
+                            node.status({fill:"green",shape:"dot",text:"true"});
+                        } else {
+                            node.status({fill:"green",shape:"ring",text:"false"});
+                        }
+                    } else {
+                        node.status({});
+                    }    
+                    msg.payload = value;
                     node.send(msg);
                 }
             }
